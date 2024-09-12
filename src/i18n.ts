59db114f1bd791ae619b9ccb59c4fa7765bd2edc
linhnvph36809 <linhnvph36809@fpt.edu.vn>
2024-09-12 16:48:02 +0700

    fix multiple language

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslation from './locales/en/translation.json';
-import viTranslation from './locales/vi/translation.json';
+import enTranslation from "../public/locales/en/translation.json"
+import viTranslation from "../public/locales/vi/translation.json"
 
 i18n
   .use(initReactI18next)
